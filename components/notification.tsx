496490f78adbcc25bb98549558cd0e61a991b8c4
PranshuBasak <pranshubasak@gmail.com>
2024-03-31 20:53:00 +0530

    deployment changes

--- components/notification.tsx
+++ components/notification.tsx
@@ -1,33 +1,33 @@
-"use client";
+// "use client";
 
-import { useCallback, useEffect } from "react";
+// import { useCallback, useEffect } from "react";
 
-const sendNotification = () => {
-    if ('Notification' in window && Notification.permission === 'granted') {
-      new Notification('Hello Developer !!', {
-        body: 'This is a notification from the website',
-        icon: '/images/notify.svg', 
-      })
-    }else{
-      requestNotificationPermission()
-    }
-  };
+// const sendNotification = () => {
+//     if ('Notification' in window && Notification.permission === 'granted') {
+//       new Notification('Hello Developer !!', {
+//         body: 'This is a notification from the website',
+//         icon: '/images/notify.svg', 
+//       })
+//     }else{
+//       requestNotificationPermission()
+//     }
+//   };
 
-  const requestNotificationPermission = useCallback(() => {
-    if ('Notification' in window) {
-      Notification.requestPermission().then(permission => {
+//   const requestNotificationPermission = useCallback(() => {
+//     if ('Notification' in window) {
+//       Notification.requestPermission().then(permission => {
         
-        if (permission === 'granted') {
-          console.log("Notification permission granted.");
-        }
-      })
-    }else{
-      console.log("This browser does not support desktop notification");
-    }
-  }, []);
+//         if (permission === 'granted') {
+//           console.log("Notification permission granted.");
+//         }
+//       })
+//     }else{
+//       console.log("This browser does not support desktop notification");
+//     }
+//   }, []);
 
-  useEffect(() => {
-    if ('Notification' in window) {
-      requestNotificationPermission();
-    }
-  }, [requestNotificationPermission]);
+//   useEffect(() => {
+//     if ('Notification' in window) {
+//       requestNotificationPermission();
+//     }
+//   }, [requestNotificationPermission]);
